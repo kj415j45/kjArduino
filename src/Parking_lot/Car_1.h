b084a4d97e16617e1f9e022dbcced78a5d99914e
kj415j45 <kj415j45@gmail.com>
2017-06-01 14:03:44 +0800

    No.1 Car //TODO

---
+++ src/Parking_lot/Car_1.h
@@ -0,0 +1,31 @@
+#ifndef KJ_CAR_1_H
+#define KJ_CAR_1_H
+
+#include "../Car.h"
+
+/*===================
+
+1号车
+ASCII字符设计图
+
+正方向
+A
+|
+|
+
+i是灰度传感，l是红外距离传感
+     ili
+  L o---o R
+      |
+      o
+    万向轮
+===================*/
+
+/**
+ * @author kj415j45
+ */
+class Car_1 : Car{
+
+};
+
+#endif

132918405218bd112bb9f9ef6e340444acef8a25
kj415j45 <kj415j45@gmail.com>
2017-06-01 14:03:13 +0800

    CarFactory and CarList

---
+++ src/CarFactory.h
@@ -0,0 +1,23 @@
+#ifndef KJ_CARF_H
+#define KJ_CARF_H
+
+#include "Car.h"
+#include "Parking_lot/CarList.h"
+
+class CarFactory{
+    private:
+
+    protected:
+
+    public:
+
+        Car getCar(int car){
+            switch(car){
+                case 1:
+                    return (Car)Car_1();
+            }
+        }
+        
+};
+
+#endif
---
+++ src/Parking_lot/CarList.h
@@ -0,0 +1,7 @@
+#ifndef KJ_CARL_H
+#define KJ_CARL_H
+
+//这个文件导入停车场(Parking_lot)所有的已知车
+#include "Car_1.h"
+
+#endif
